1c1e61813a144aa1145865fc02c23c0d9f22b542
jjmckenzie <>
2018-06-19 00:04:15 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 var friendsData = require("../data/friends");
-let newScore = [];
+
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
@@ -8,24 +8,7 @@ module.exports = function(app) {
 
     //Create new friend
     app.post("/api/friends", function(req, res) {
-
-
-        for (key in req.body.scores) {
-            newScore.push(parseInt(req.body.scores[key]));
-        }
-
-        let newFriend = {
-            name: req.body.name, 
-            photo: req.body.photo,
-            scores: newScore
-        }
-
-        friendsData.push(newFriend);
-
-        console.log(newScore);
-        
-
-        res.json("Success");
-    })
+        friendsData.push(req.body);
+    });
 
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
+module.exports = function(app) {
+    //Basic route that send the user to the first AJAX Page
+    app.get("/", function(req, res) {
+        console.log("success")
+        res.sendFile(path.join(__dirname, "../public/home.html"))
+    })
 
-//Basic route that send the user to the first AJAX Page
-app.get("/", function(req, res) {
-    console.log("success")
-    res.sendFile(path.join(__dirname, "./public/home.html"))
-})
+    //Basic route to go to the reserve page
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"))
+    })
 
-//Basic route to go to the reserve page
-app.get("/survey", function(req, res) {
-    res.sendFile(path.json(__dirname, "./public/survey.html"))
-})
+}
